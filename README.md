278bb84d0dd3b7d03d1397d1a38a3d8df1836e5f
Leandro Ostera <sayhi@leostera.com>
2013-09-21 03:54:05 -0300

    release(0.1.0): new interface.
    support for function, object or array of transports.
    
    chore(readme): updated readme.
    
    chore(version): bumped version up to 0.1.0 to republish on npm.

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
 # Anchorman
-### a reporter transport library over events
-
+### a reporter library
 ![](https://si0.twimg.com/profile_images/1990249248/image.jpg)
 
-### Actual interface
-```js
-var anchorman = require('anchorman');
-anchorman.on('someEvent', transportsArray);
-anchorman.emit('someEvent', {some: 'data'});
-```
+### Motivation
+I made this lib for SequelizeJS so they could easily hook up mocha reporters but it never happened. Dead small, dead simple and it works over events which is the way I considered best to be done. That's about the size of it.
 
-### Proposed interface
+### Usage
 ```js
 // opts is an array of options
 // that includes an eventemitter2 attribute
@@ -18,11 +13,32 @@ anchorman.emit('someEvent', {some: 'data'});
 var anchorman = require('anchorman')(opts);
 
 // let's subscribe to some news
-anchorman.on('someEvent', transportsArray);
+anchorman.on('someEvent', transports);
 
 // now lets broadcast some news
 anchorman.broadcast('someEvent', {some: 'data'});
-```
 
-### Credits
-Made with :heart: by Leandro Ostera
+// transports can be just a function
+anchorman.on('someOtherEvent', function (data) {
+  console.log(data);
+});
+
+// transports can be objects that implement a publish function
+var transportObject = {
+  someData: 'blabla',
+  publish: function (data) {
+
+  }
+};
+
+anchorman.on('someOtherEvent', transportObject);
+
+// or they can be an array of transport objects
+var transportsArray = [
+  consoleTransport,
+  fileTransport,
+  emailTransport
+];
+
+anchorman.on('someEvent', transportsArray);
+```
--- index.js
+++ index.js
@@ -1,44 +1,54 @@
-var EventEmitter2 = require('eventemitter2').EventEmitter2;
-
-var e = new EventEmitter2({
+var ee2 = require('eventemitter2').EventEmitter2;
+var e = new ee2({
   newListeners: false,
   delimiter:':'
-});
+})
 
-module.exports = {
+var anchorman = {
   prefix: 'anchorman:',
   channels: {},
 
-  emit: function (channelName, data) {
-    if(!this.channels[channelName]) {
-      throw new Error('Failed to emit to a channel without any transports to handle it');
+  broadcast: function (channel, data) {
+    if(!this.channels[this.prefix+channel]) {
+      throw new Error('Failed to broadcast to a channel without any transports to handle it');
     }
-    e.emit(this.prefix+channelName, data);
+    e.emit(this.prefix+channel, data);
     return this;
   },
 
-  on: function (channelName, transports) {
-    if(channelName) {
-      channelName = this.prefix+channelName;
+  on: function (channel, transports) {
+    if(!e) throw new Error('No event emitter?!');
+    if(channel) {
+      channel = this.prefix+channel;
       if(!this.channels 
-        || !this.channels[channelName]) {
-        this.channels[channelName] = [];
+        || !this.channels[channel]) {
+        this.channels[channel] = [];
+        e.on(channel, function (data) {
+          if(this.channels[channel]) {
+            console.log(channel, this.channels[channel]);
+            this.channels[channel].forEach(function (t) {
+              t.publish(data);
+            });
+          }
+        }.bind(this));
       }
       if(Array.isArray(transports)) {
         transports.forEach(function (t) {
-          this.channels[channelName].push(t);
+          this.channels[channel].push(t);
         }.bind(this));
+      } else if (typeof transports === 'function') {
+        this.channels[channel].push({
+          publish: transports
+        });
       } else {
-        this.channels[channelName].push(transports);
+        this.channels[channel].push(transports);
       }
-      e.on(channelName, function (data) {
-        if(this.channels[channelName]) {
-          this.channels[channelName].forEach(function (t) {
-            t.publish(data);
-          });
-        }
-      }.bind(this));
     }
     return this;
   }
 };
+
+// inherit from EventEmitter2
+// require('utils').inherit(anchorman,require('eventemitter2').EventEmitter2);
+
+module.exports = anchorman;
